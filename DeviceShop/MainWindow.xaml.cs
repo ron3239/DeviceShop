a8cdf8002fbefb1731bbb48b4459e9e7c6dafeff
ron3239 <dawntrail@mail.ru>
2025-04-01 14:45:59 +0300

    wpf create

---
+++ DeviceShop/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DeviceShop.components.screen;
+using System.Windows;
+
+namespace DeviceShop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Main());
+        }
+    }
+}
